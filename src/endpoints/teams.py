35e0b5f5f412fd6600cd2b1c30b389eabfa0368b
david-chen0 <2020davidgolf@gmail.com>
2025-06-17 19:44:46 -0700

    small commit

---
+++ src/endpoints/teams.py
@@ -0,0 +1,10 @@
+from util.scraper import *
+from datetime import datetime
+
+def get_team(scraper: HLTVScraper, team_name: str, time: datetime) -> dict:
+    """
+    Returns info for the given team name at the specified point in time. Returned dict will have the following fields:
+
+    
+    """
+    return
